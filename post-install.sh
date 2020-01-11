0e10752b0db3439f812e1148812eab0241313387
Evan Wegley <tomocafe@pm.me>
2020-01-10 21:49:46 -0800

    Fix post-install script

--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gtk-update-icon-cache -q -t -f "${MESON_INSTALL_PREFIX:-/usr}/share/icons/hicolor"
+sudo gtk-update-icon-cache -q -t -f "${MESON_INSTALL_PREFIX:-/usr}/share/icons/hicolor"
 
