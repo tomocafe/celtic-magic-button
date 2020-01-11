84890e80df2d7ee9747b0ae09b13c4da4d0c1554
Evan Wegley <tomocafe@pm.me>
2020-01-10 21:57:43 -0800

    Revert "Fix post-install script"
    This reverts commit 0e10752b0db3439f812e1148812eab0241313387.

--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo gtk-update-icon-cache -q -t -f "${MESON_INSTALL_PREFIX:-/usr}/share/icons/hicolor"
+gtk-update-icon-cache -q -t -f "${MESON_INSTALL_PREFIX:-/usr}/share/icons/hicolor"
 
